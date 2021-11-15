0e6f726eb4b94597ac9975159c731123b3b52cb0
Juan <juanpabloguleal@gmail.com>
2021-11-14 23:44:42 -0600

    Locator and MainVM were added

---
+++ SyncfusionSamples/Services/Locator.cs
@@ -0,0 +1,21 @@
+﻿using SyncfusionSamples.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncfusionSamples.Services
+{
+    public class Locator
+    {
+        #region ViewModels
+        public MainViewModel Main { get; set; }
+        #endregion
+        #region Constructor
+        public Locator()
+        {
+            Main = new MainViewModel();
+        }
+        #endregion
+
+    }
+}
---
+++ SyncfusionSamples/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncfusionSamples.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+
+        #endregion
+        #region Constructor
+        public MainViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
