f8aa0b912e5bc19d0d98d9e6513ebdb2c63370e2
Juan <juanpabloguleal@gmail.com>
2021-11-14 23:32:41 -0600

    AppShell Added

--- SyncfusionSamples/App.xaml.cs
+++ SyncfusionSamples/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SyncfusionSamples.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace SyncfusionSamples
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
